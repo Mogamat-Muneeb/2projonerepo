db35cbec5259701625c919b1253d926f0eeededc
Mogamat-Muneeb <davimuneeb785@gmail.com>
2024-03-04 11:35:33 +0200

    wip:backend and frontend done

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+// Import Express
+const express = require("express");
+const cors = require("cors");
+// Create Express app
+const app = express();
+app.use(express.json());
+app.use(cors());
+// Define a route
+app.get("/", (req, res) => {
+  res.send("Hello, Worldddd!");
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      message: "",
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetch("http://localhost:3000");
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        const data = await response.text();
+        this.message = data;
+      } catch (error) {
+        console.error("There was a problem fetching the data:", error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Your styles here */
+</style>
